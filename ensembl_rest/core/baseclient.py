0fffa4769097b0491058e76e5b2fbc7d536ccf19
Robert Ernst <r.f.ernst-3@umcutrecht.nl>
2023-01-12 11:55:16 +0100

    Fix imports

--- ensembl_rest/core/baseclient.py
+++ ensembl_rest/core/baseclient.py
@@ -6,29 +6,32 @@ This file is part of pyEnsembl.
     Any questions, comments or issues can be addressed to a.garcia230395@gmail.com.
 
 """
+import pprint
+import sys
+import time
 
 from .restclient import RESTClient, HTTPError
-import pprint
+
 
 
 class BaseEnsemblRESTClient:
     """Base client for an Ensembl REST API."""
-    
+
     def __init__(self, base_url=None):
         if base_url is None:
             base_url = self.base_url
-            
+
         self.rest_client = RESTClient(base_url)
     # ----
-        
+
     def make_request(self, resource, *args, params=None, headers=None, **kwargs):
         "Follow the route mapping the arguments to the correct place."
         # Assemble the request
         request_type, route_template = resource.split(' ')
         route = self._map_arguments(route_template, *args, **kwargs)
-        
+
         endpoint = self.rest_client.route(*route)
-        
+
         # Perform the request
         while True:
             try:
@@ -41,33 +44,33 @@ class BaseEnsemblRESTClient:
                         # Need to wait
                         response = e.response
                         wait_time = float(response.headers["Retry-After"])
-                        sys.stderr.write('Maximum requests limit reached, waiting for ' 
+                        sys.stderr.write('Maximum requests limit reached, waiting for '
                                          + str(wait_time)
-                                         + 'secs')
+                                         + 'secs\n')
                         time.sleep(wait_time)
                     else:
                         raise
-    # ---        
-        
+    # ---
+
     def _map_arguments(self, route, *args, **kwargs):
         """Map the arguments to the template.
-        
+
         The template is a string of the form:
         "some/:string/with/:semicolons"
-        
+
         First maps the **kwargs, then maps the *args in order.
         If this fails throw an exception.
-        
+
         """
-        # First split into arguments: 
-        # "some/:string/with/:semicolons" -> ['some', ':string', 'with', ':semicolons'] 
+        # First split into arguments:
+        # "some/:string/with/:semicolons" -> ['some', ':string', 'with', ':semicolons']
         parts = route.split('/')
         # Then replace the semicolons with format strings
         parts = ['{' + s[1:] + '}' if s.startswith(':') else s for s in parts]
-        
+
         # Then map the keyword arguments
         parts = [self._format_if_possible(s, **kwargs) for s in parts]
-        
+
         # Finally, map the positional arguments
         args_iter = iter(args)
         mapped_parts = []
@@ -80,13 +83,13 @@ class BaseEnsemblRESTClient:
                     pass
             else:
                 mapped_parts.append(s)
-        
+
         return mapped_parts
     # ---
-           
+
     def _format_if_possible(self, format_string, **kwargs):
         """Try to apply the arguments to the format string.
-        
+
         If not possible, return the string unchanged.
         """
         try:
@@ -94,7 +97,7 @@ class BaseEnsemblRESTClient:
         except KeyError:
             return format_string
     # ---
-    
+
 # --- BaseClient
 
 
@@ -111,7 +114,7 @@ def _format_parameters_docstring(parameters):
     parameters_str = ''
     for parameter in parameters:
         parameters_str += f'  + *Name*: {parameter["Name"]}\n\n'
-        
+
         for field in fields:
             parameters_str += f'    * *{field}*: {parameter[field]}\n'
 
@@ -137,14 +140,14 @@ def _endpoint_docstring(endpoint):
 
 def _create_method(method_name, endpoint):
     """Create a class method"""
-    
+
     def method(self, *args, **kwargs):
         return self.make_request(endpoint['resource_string'], *args, **kwargs)
     # ---
-    
+
     method.__doc__ = _endpoint_docstring(endpoint)
     method.__name__ = method_name
-    
+
     return method
 # ---
 
@@ -153,13 +156,13 @@ def build_client_class(name, api_table, base_url, doc=''):
     # Create the class dictionary
     class_dict = {'__doc__': doc,
                   'base_url': base_url}
-    
+
     # Create the class methods
-    methods = {endpoint['name'] : _create_method(endpoint['name'], endpoint) 
+    methods = {endpoint['name'] : _create_method(endpoint['name'], endpoint)
                for endpoint in api_table}
-    
+
     class_dict.update(methods)
-    
+
     # Create the class (a subclass of the BaseEnsemblRESTClient)
     return type(name, (BaseEnsemblRESTClient,), class_dict)
 # ---
